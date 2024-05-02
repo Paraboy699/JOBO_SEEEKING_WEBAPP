a67ad72ab6befacbd8c5a1bc594278c34ebee7d6
Pukar Singh <spukar133@gmail.com>
2024-05-03 00:12:42 +0545

    Work on logout

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -40,3 +40,14 @@ export const login = catchAsyncError(async (req, res, next) => {
   }
   sendToken(user, 200, res, "User logged in successfully");
 });
+
+export const logout = catchAsyncError(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    message: "Logged out successfully !",
+  });
+});
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { register, login } from "../controllers/userController.js";
+import { register, login, logout } from "../controllers/userController.js";
 
 const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
+router.get("/logout", logout);
 
 router.get("/", (req, res) => {
   res.send("Hello World!");
