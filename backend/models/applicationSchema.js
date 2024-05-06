c80c808f029f1c8050b5eb170fb00653f3286345
Pukar Singh <spukar133@gmail.com>
2024-05-07 02:15:09 +0545

    Application Schema

---
+++ backend/models/applicationSchema.js
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+import mongoosefro from "mongoose";
+import validator from "validator";
+
+const applicationSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+  coverLetter: {
+    type: String,
+    required: [true, "Please provide your cover letter"],
+  },
+  phone: {
+    type: String,
+    required: [true, "Please provide your phone number"],
+  },
+  address: {
+    type: String,
+    required: [true, "Please provide your address"],
+  },
+  resume: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  applicantID: {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["Job Seeker"],
+      required: true,
+    },
+  },
+  employerID: {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["Employer"],
+      required: true,
+    },
+  },
+});
+
+export const Application = mongoose.model("Application", applicationSchema);
