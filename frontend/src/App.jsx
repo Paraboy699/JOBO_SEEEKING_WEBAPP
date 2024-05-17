0380b2d18f1ad022ebd051db9153a5dbca847903
Pukar Singh <spukar133@gmail.com>
2024-05-17 16:54:35 +0545

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,6 @@ import MyApplications from "./components/Application/MyApplications";
 import PostJob from "./components/Job/PostJob";
 import NotFound from "./components/NotFound/NotFound";
 import MyJobs from "./components/Job/MyJobs";
-import { Navigate } from "react-router-dom";
 
 const App = () => {
   const { isAuthorized, setIsAuthorized, setUser } = useContext(Context);
@@ -29,15 +28,12 @@ const App = () => {
             withCredentials: true,
           }
         );
-        console.log(response); // Log the full response to the console
         setUser(response.data.user);
         setIsAuthorized(true);
       } catch (error) {
-        console.log(error); // Log any errors to the console
         setIsAuthorized(false);
       }
     };
-
     fetchUser();
   }, [isAuthorized]);
 
@@ -48,39 +44,15 @@ const App = () => {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route
-            path="/"
-            element={isAuthorized ? <Home /> : <Navigate to="/login" />}
-          />
-          <Route
-            path="/job/getall"
-            element={isAuthorized ? <Jobs /> : <Navigate to="/login" />}
-          />
-          <Route
-            path="/job/:id"
-            element={isAuthorized ? <JobDetails /> : <Navigate to="/login" />}
-          />
-          <Route
-            path="/application/:id"
-            element={isAuthorized ? <Application /> : <Navigate to="/login" />}
-          />
-          <Route
-            path="/applications/me"
-            element={
-              isAuthorized ? <MyApplications /> : <Navigate to="/login" />
-            }
-          />
-          <Route
-            path="/job/post"
-            element={isAuthorized ? <PostJob /> : <Navigate to="/login" />}
-          />
-          <Route
-            path="/job/me"
-            element={isAuthorized ? <MyJobs /> : <Navigate to="/login" />}
-          />
+          <Route path="/" element={<Home />} />
+          <Route path="/job/getall" element={<Jobs />} />
+          <Route path="/job/:id" element={<JobDetails />} />
+          <Route path="/application/:id" element={<Application />} />
+          <Route path="/applications/me" element={<MyApplications />} />
+          <Route path="/job/post" element={<PostJob />} />
+          <Route path="/job/me" element={<MyJobs />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-
         <Footer />
         <Toaster />
       </BrowserRouter>
